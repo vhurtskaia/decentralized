e98db9582cdbcccebb7f7e01f25490df9723d74d
vazha <gudenaffalkon@gmail.com>
2025-06-27 22:40:20 +0200

    bigfix

--- src/widgets/MainSection/ui/MainSection.tsx
+++ src/widgets/MainSection/ui/MainSection.tsx
@@ -7,11 +7,18 @@ import styles from "./MainSection.module.css";
 
 export const MainSection = () : JSX.Element => {
     return (
-        <section className={styles.mainSection}>
+        <>
+            {/*<Image*/}
+            {/*    src={'/images/widgets/ProjectsIntegratedSection/planet.jpg'}*/}
+            {/*    className={styles.planet}*/}
+            {/*    height={1016}*/}
+            {/*    width={1016}*/}
+            {/*    alt={'Planet Decoration'}*/}
+            {/*/>*/}
+
+            <section className={styles.section}>
                 <h1>A new economic primitive for funding decentralized AI</h1>
-                <p className={styles.description}>
-                    We track, rank and pay for the best open source decentralized LLMs to compete against OpenAI
-                </p>
+                <p>We track, rank and pay for the best open source decentralized LLMs to compete against OpenAI</p>
 
                 <div className={styles.buttons}>
                     <Button bg size={'big'}>
@@ -21,23 +28,59 @@ export const MainSection = () : JSX.Element => {
                     <Button size={'big'}>
                         Try Now
                     </Button>
-
                 </div>
 
                 <div className={styles.stats}>
-                    <div className={styles.statsBlock}>
-                        <p className={styles.statsTitle}>1,873</p>
-                        <p className={styles.statsText}>LLM models</p>
+                    <div className={styles.stats__block}>
+                        <p className={styles.stats__title}>1,873</p>
+                        <p>LLM models</p>
                     </div>
-                    <div className={styles.statsBlock}>
-                        <p className={styles.statsTitle}>$326,734</p>
-                        <p className={styles.statsText}>Paid to data scientists</p>
+                    <div className={styles.stats__block}>
+                        <p className={styles.stats__title}>$326,734</p>
+                        <p>Paid to data scientists</p>
                     </div>
-                    <div className={styles.statsBlock}>
-                        <p className={styles.statsTitle}>6,557</p>
-                        <p className={styles.statsText}>Developers</p>
+                    <div className={styles.stats__block}>
+                        <p className={styles.stats__title}>6,557</p>
+                        <p>Developers</p>
                     </div>
                 </div>
             </section>
+
+            <section>
+                <div>
+                    <h2>LLM Leaderboard</h2>
+                    <Button bg size={'big'}>
+                        Submit your model
+                    </Button>
+                </div>
+                <p>We evaluate LLMs on key benchmarks using the Eleuther AI, a framework to test LLMs on a large number of different evaluation tasks.
+                    The higher the score, the better the LLM.</p>
+                <div>
+                    here is a table
+                </div>
+            </section>
+
+            <article>
+                <p>Join our community and harvest $SALT</p>
+            </article>
+
+            <Image
+                src={'/images/widgets/ProjectsIntegratedSection/BlueDecoration.svg'}
+                className={styles.decoration__blue}
+                height={504}
+                width={796}
+                alt={'Blue Decoration'}
+            />
+
+            <Image
+                src={'/images/widgets/ProjectsIntegratedSection/OrangeDecoration.svg'}
+                className={styles.decoration__orange}
+                height={504}
+                width={796}
+                alt={'Orange Decoration'}
+            />
+
+
+        </>
     );
 };
