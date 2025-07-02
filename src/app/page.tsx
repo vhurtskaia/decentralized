e2a86b46f5324f355573f54a24eded43e9d046cc
vazha <gudenaffalkon@gmail.com>
2025-07-02 05:03:55 +0200

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,20 @@
+'use client'
+
 import {JSX} from "react";
 
 import {
     Header,
-    IntroSection,
-    ProjectsIntegratedSection,
-    CrowdsourcingSection,
-    JoinSection,
     Footer,
-    LeaderboardSection,
-    HarvestSection,
 } from "@/widgets";
 
-import styles from "./page.module.css";
+import {PageSlider} from "@/widgets/PageSlider";
 
 export default function Home(): JSX.Element {
     return (
-        <div className={styles.wrapper}>
-            <Header/>
-            <main>
-                <IntroSection/>
-                <ProjectsIntegratedSection/>
-                <CrowdsourcingSection/>
-                <LeaderboardSection/>
-                <JoinSection/>
-                <HarvestSection/>
-            </main>
-            <Footer/>
-        </div>
+        <>
+            <Header />
+            <PageSlider />
+            <Footer />
+        </>
     );
 }
